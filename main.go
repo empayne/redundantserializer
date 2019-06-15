a6ca3be20cecf7751d85b7578e585a345051d524
Eric Payne <eric.michael.payne@gmail.com>
2019-06-15 14:50:32 -0400

    Implemented serialization; reduced scope from map[string]interface{} to map[string]string

--- main.go
+++ main.go
@@ -4,50 +4,92 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/gob"
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 )
 
 type redundantStructure struct {
-	XMLString  string
-	JSONString string
+	XMLData  []byte
+	JSONData []byte
 }
+
+// SerializableMap is used to define map[string]string <-> XML (un)marshalling,
+// as Golang's 'encoding/xml' doesn't natively support this.
+type SerializableMap map[string]string
 
 func main() {
 	type Foo struct {
-		score int
+		score string
 		bio   string
 	}
-	testStruct := Foo{score: 1, bio: "text"}
-	testMSI := map[string]interface{}{"score": testStruct.score, "bio": testStruct.bio}
-	serialized := serialize(testMSI)
-	fmt.Println(serialized)
+	testStruct := Foo{score: "1", bio: "text"}
+	testMSS := map[string]string{"score": testStruct.score, "bio": testStruct.bio}
+
+	serialized, err := serialize(testMSS)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(*serialized)
+	}
 }
 
-func serialize(in map[string]interface{}) string {
-	jsonString := getJSONString(in)
-	xmlString := getXMLString(in)
-	toSerialize := redundantStructure{jsonString, xmlString}
-	serialized := toGOB64(toSerialize)
-	return serialized
+func serialize(in SerializableMap) (*string, error) {
+	xmlData, err := xml.MarshalIndent(in, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+
+	jsonData, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	toSerialize := redundantStructure{xmlData, jsonData}
+	return toGoB64(toSerialize)
 }
 
-func getJSONString(in map[string]interface{}) string {
-	return "unimplemented json"
+// MarshalXML maps our SerializableMap to XML data.
+// Taken from https://stackoverflow.com/questions/30928770
+func (s SerializableMap) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+
+	tokens := []xml.Token{start}
+
+	for key, value := range s {
+		t := xml.StartElement{Name: xml.Name{"", key}}
+		tokens = append(tokens, t, xml.CharData(value), xml.EndElement{t.Name})
+	}
+
+	tokens = append(tokens, xml.EndElement{start.Name})
+
+	for _, t := range tokens {
+		err := e.EncodeToken(t)
+		if err != nil {
+			return err
+		}
+	}
+
+	// flush to ensure tokens are written
+	err := e.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func getXMLString(in map[string]interface{}) string {
-	return "unimplemented xml"
-}
-
-// go binary encoder, inspired by https://stackoverflow.com/questions/28020070/
-func toGOB64(m redundantStructure) string {
+// Go base64 encoder, inspired by https://stackoverflow.com/questions/28020070
+func toGoB64(m redundantStructure) (*string, error) {
 	b := bytes.Buffer{}
 	e := gob.NewEncoder(&b)
+
 	// TODO: potential optimization by not calling gob.Register every time
 	gob.Register(redundantStructure{})
 	err := e.Encode(m)
 	if err != nil {
-		fmt.Println(`failed gob Encode`, err)
+		return nil, err
 	}
-	return base64.StdEncoding.EncodeToString(b.Bytes())
+
+	encoded := base64.StdEncoding.EncodeToString(b.Bytes())
+	return &encoded, nil
 }
